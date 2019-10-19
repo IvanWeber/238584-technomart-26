ee18de1ef658d1ee18ea279168da82bb8dc432d7
Ivan Weber <b24b24testing.ru>
2019-10-19 20:45:16 +0300

    Доработано модальное окно добавление товара в корзину, теперь можно нажать на кнопку "Продолжить покупки" для закрытия окна

--- js/added-to-cart-window.js
+++ js/added-to-cart-window.js
@@ -1,6 +1,7 @@
 const addButton = document.querySelectorAll('.popular-products__product-add-to-cart');
 const modalAdd = document.querySelector('.modal-add');
 const addCloseButton = modalAdd.querySelector('.modal-add-close');
+const toPurchasesButton = modalAdd.querySelector('.to-purchases');
 
 
 const addButtonClickHandler = () => {
@@ -17,6 +18,10 @@ const modalAddEscKeydownHandler = (evt) => {
   }
 };
 
+const toPurchasesButtonClickHandler = () => {
+  modalAdd.classList.add('visually-hidden');
+};
+
 
 addButton.forEach((el) => {
   el.addEventListener('click', addButtonClickHandler);
@@ -24,3 +29,4 @@ addButton.forEach((el) => {
 
 addCloseButton.addEventListener('click', addCloseButtonClickHandler);
 document.addEventListener('keydown', modalAddEscKeydownHandler);
+toPurchasesButton.addEventListener('click', toPurchasesButtonClickHandler);
